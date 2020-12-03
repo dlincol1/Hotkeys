9a5bd8c9ed8c944547eb35835397a57dd73e4184
Daniel Lincoln <39199224+dlincol1@users.noreply.github.com>
2020-12-03 12:36:39 -0500

    Add files via upload

---
+++ Hotkeys.py
@@ -0,0 +1,42 @@
+# Gets Hotkey assignments for use in Hotkeys.html
+# Copy this to VRED VariantSet Script tab and name as 'Hotkeys'
+# By Dan Lincoln 11/15/2015
+
+print "Getting Hotkeys..."
+print "Supported Keys for this version (0-9, A-Z):"
+vsetPtrs = getVariantSets()
+
+# Build a list of names for iteration, cannot just iterate on the Ptr names
+vsets = []
+hkeys = []
+keys_active = []
+key_numbers = []
+keys_supported = [
+'1','2','3','4','5','6','7','8','9','0',
+'A','B','C','D','E','F','G','H','I','J','K','L','M',
+'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+# Dictionary of keys & values
+hotkeyDictionary = {}
+
+for i in vsetPtrs:
+    vsets.append(i)
+
+for j in vsets:
+    v = getVariantSet(j)
+    h = v.getHotkey()
+
+    if h != "":
+        # remove spaces
+        dictKey = h.strip()
+        hkeys.append(h.strip())
+        if len(h.strip()) == 1:
+            keys_active.append(h.strip()) 
+        hotkeyDictionary[dictKey] = j
+
+hkeys.sort()
+keys_active.sort()
+
+print "Hotkeys found: " + str(hkeys)
+print "Dictionary keys: " + str(hotkeyDictionary.keys())
+print "Dictionary values: " + str(hotkeyDictionary.values())
